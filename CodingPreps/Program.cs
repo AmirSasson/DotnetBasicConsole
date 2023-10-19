90162a3581af356d4fa041aee6325758f2957ec3
AmirSasson <amirsas@gmail.com>
2023-10-19 11:08:07 +0300

    with command line parser

--- CodingPreps/Program.cs
+++ CodingPreps/Program.cs
@@ -3,60 +3,72 @@ using CodingPreps;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.CommandLine;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+var delayArgument = new Argument<int>
+    (name: "delay",
+    description: "delay between upserts",
+    getDefaultValue: () => 500);
 
-builder.Services.AddTransient<IPersonsService, PersonsService>();
-builder.Services.AddSingleton<IPersonsRepository, InMemPersonsRepository>();
-builder.Logging.AddConsole();
-
-
-using IHost host = builder.Build();
-CancellationTokenSource tokenSrc = new CancellationTokenSource();
-
-
-
-var loggerFac = host.Services.GetService<ILoggerFactory>();
-var logger = loggerFac.CreateLogger<PersonsService>();
-
-Console.CancelKeyPress += (sender, eArgs) =>
+var rootCommand = new RootCommand
 {
-    tokenSrc.Cancel();
-    logger.LogCritical("Shutting Down requested..");
-    eArgs.Cancel = true;
+    delayArgument
 };
-// kick off asynchronous stuff 
 
-
-logger.LogCritical("Staring.");
-
-
-Task.Run(async () =>
+rootCommand.SetHandler((delayArgumentValue) =>
 {
-    while (!tokenSrc.IsCancellationRequested)
+    HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddTransient<IPersonsService, PersonsService>();
+    builder.Services.AddSingleton<IPersonsRepository, InMemPersonsRepository>();
+    builder.Logging.AddConsole();
+
+
+    using IHost host = builder.Build();
+    CancellationTokenSource tokenSrc = new CancellationTokenSource();
+
+    var loggerFactory = host.Services.GetService<ILoggerFactory>();
+    var logger = loggerFactory!.CreateLogger<PersonsService>();
+
+    Console.CancelKeyPress += (sender, eArgs) =>
     {
-        var person = new Person(DateTime.UtcNow.Ticks, $"amir-{DateTime.UtcNow.Ticks}", "sasson");
+        tokenSrc.Cancel();
+        logger.LogCritical("Shutting Down requested..");
+        eArgs.Cancel = true;
+    };
+    
+    logger.LogCritical("Staring engine.");
 
-        var service = host.Services.GetService<IPersonsService>();
-
-        await service!.Upsert(person);
-
-        await Task.Delay(200);
-    }
-});
-
-
-Task.Run(async () =>
-{
-    while (!tokenSrc.IsCancellationRequested)
+    Task.Run(async () =>
     {
-        var service = host.Services.GetService<IPersonsService>();
+        while (!tokenSrc.IsCancellationRequested)
+        {
+            var person = new Person(DateTime.UtcNow.Ticks, $"amir-{DateTime.UtcNow.Ticks}", "sasson");
 
-        var persons = await service!.GetAll();
-        logger.LogInformation($"has {persons.Count()}");
+            var service = host.Services.GetService<IPersonsService>();
 
-        await Task.Delay(1000);
-    }
-});
+            await service!.Upsert(person);
 
-tokenSrc.Token.WaitHandle.WaitOne();
+            await Task.Delay(delayArgumentValue);
+        }
+    });
+
+
+    Task.Run(async () =>
+    {
+        while (!tokenSrc.IsCancellationRequested)
+        {
+            var service = host.Services.GetService<IPersonsService>();
+
+            var persons = await service!.GetAll();
+            logger.LogInformation($"has {persons.Count()}");
+
+            await Task.Delay(1000);
+        }
+    });
+
+    tokenSrc.Token.WaitHandle.WaitOne();
+
+},delayArgument);
+
+await rootCommand.InvokeAsync(args);
